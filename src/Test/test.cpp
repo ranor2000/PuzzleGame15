437913a711a8fadcd50fc37ba159ebf89cfd7001
Dolbilov Alexandr <dolbilov53@gmail.com>
2019-09-16 03:56:39 +0700

    Refactory

--- src/Test/test.cpp
+++ src/Test/test.cpp
@@ -1,29 +1,34 @@
 #include "../Puzzle/generation.h"
 #include "../Puzzle/play.h" 
 #include "../Puzzle/shift.h"
+
 #include "pch.h"
 
 TEST(GeneralFunctions, Swap) {
-	byte a = 1, b = 2;
-	swap(a, b);
+	byte a, da, b, db;
+	a = da = 1,
+	b = db = 2;
+	swap(da, db);
+
+	ASSERT_NE(da, a);
+	ASSERT_NE(db, b);
 }
 
 TEST(GameMechanics, Generation) {
 	byte** testField;
 
-	for (int k = 0; k < 100; k++) {
-		testField = GenerateNewField();
-		int sum = 0;
+	testField = GenerateNewField();
+	int sum = 0;
 
-		// checking for control sum
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 4; j++)
-				sum += testField[i][j];
-		}
-
-		DeleteField(testField);
-		ASSERT_EQ(sum, 120);
+	// checking for control sum
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++)
+			sum += testField[i][j];
 	}
+
+	DeleteField(testField);
+
+	ASSERT_EQ(sum, 120);
 }
 
 TEST(GameMechanics, Victory) {
